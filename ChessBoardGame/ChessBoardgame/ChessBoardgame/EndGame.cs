f55d5a4753b33e6acefb31860598b6c5da9a776b
Somaiya <somaiyajannat044@gmail.com>
2023-03-11 12:08:59 +0600

    Add

---
+++ ChessBoardGame/ChessBoardgame/ChessBoardgame/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardgame
+{
+    public class EndGame
+    {
+        public string Confirm = null;
+        public void EndGameFun(string Confirm)
+        {
+            Console.WriteLine("Are you sure to End The Game?");
+            
+        }
+    }
+}
